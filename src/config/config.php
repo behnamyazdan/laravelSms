aa31ddf0d7b7baef3f37d53a8d02a47a4c027ac8
Farzad Yazdan <livanaweb@gmail.com>
2014-07-29 22:42:50 +0430

    change configuration data

--- src/config/config.php
+++ src/config/config.php
@@ -3,9 +3,9 @@
 return array (
 
 	'webserviceUrl' => 'http://sms.parssms24.ir/post/send.asmx?wsdl',
-	'username'		=> '9383116711',
-	'password'		=> '09383116711',
-	'from'			=> '30002666626221',
+	'username'		=> '9123456789',
+	'password'		=> '****',
+	'from'			=> '****',
 	'isflash'		=> false,
 	'udh'			=> '',
 	'recId'			=> array(0),
